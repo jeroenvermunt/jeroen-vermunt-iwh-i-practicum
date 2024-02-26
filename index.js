9cd52637e4a04c0fa5e5a3edd17ef2fb9f9e2d7e
jeroenvermunt <jeroencv7@gmail.com>
2024-02-26 12:06:02 +0100

    completed assignment

--- index.js
+++ index.js
@@ -7,20 +7,69 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// * get .env file
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => { 
+  
+    const get_pets = 'https://api.hubspot.com/crm/v3/objects/2-123864235?properties=pet_name,pet_type,preferred_food';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(get_pets, { headers });
+        const data = resp.data.results;
+
+        res.render('homepage', { title: 'Custom Objects | Integrating With HubSpot I Practicum', data });
+
+    } catch (error) {
+        console.error(error);
+    }
+
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });   
+
+})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+
+    const create_pet = 'https://api.hubspot.com/crm/v3/objects/2-123864235';
+
+    const update = {
+        properties: {
+            "pet_name": req.body.name,
+            "pet_type": req.body.type,
+            "preferred_food": req.body.preferred_food,
+        }
+    }
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.post(create_pet, update, { headers });
+        const data = resp.data.results;
+
+        res.redirect('/');
+           
+    } catch (error) {
+        console.error(error);
+    }
+
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
